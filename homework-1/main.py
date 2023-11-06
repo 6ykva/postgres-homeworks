b20825fdaca8de256716fa5825fb9eca7a4ca5ad
alexandr <6ykva.izh@gmail.com>
2023-11-06 22:39:03 +0400

    Homework 1

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,37 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+with open('north_data/employees_data.csv') as file:
+    employees_data = list(csv.reader(file))
+
+with open('north_data/customers_data.csv') as file:
+    customers_data = list(csv.reader(file))
+
+with open('north_data/orders_data.csv') as file:
+    orders_data = list(csv.reader(file))
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="bykva",
+    password="password"
+)
+
+with conn:
+    with conn.cursor() as cur:
+
+        cur.executemany('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', employees_data[1:])
+        cur.execute("SELECT * FROM employees")
+
+        cur.executemany('INSERT INTO customers VALUES (%s, %s, %s)', customers_data[1:])
+        cur.execute("SELECT * FROM customers")
+
+        cur.executemany('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', orders_data[1:])
+        cur.execute("SELECT * FROM orders")
+
+        # запускаем все считывания
+        rows = cur.fetchall()
+
+conn.close()
